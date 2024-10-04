133f1130992e76ec19f7aa0e5972f71ed51b2c27
Shifananasrin <shifananasrin941@gmail.com>
2024-10-04 11:43:31 +0530

    Delete swap_without_temp_01_10.py

--- swap_without_temp_01_10.py
+++
@@ -1,9 +0,0 @@
-A=int(input("enter the number:"))
-B=int(input("enter the number:"))
-print("before swapping")
-print("A= " ,A," B=",B)
-A=A+B
-B=A-B
-A=A-B
-print("After swapping")
-print("A= " ,A," B=",B)
