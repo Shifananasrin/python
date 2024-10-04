11983616860dc4171fb89b7f678ba45c8f9ab3ae
Shifananasrin <shifananasrin941@gmail.com>
2024-10-04 11:56:47 +0530

    Add files via upload

---
+++ month 10/week1/increment_decrement_01_10.py
@@ -0,0 +1,4 @@
+A=int(input("enter the number"))
+A=A+5
+B=A-3
+print(A, B)
---
+++ month 10/week1/swap_without_temp_01_10.py
@@ -0,0 +1,9 @@
+A=int(input("enter the number:"))
+B=int(input("enter the number:"))
+print("before swapping")
+print("A= " ,A," B=",B)
+A=A+B
+B=A-B
+A=A-B
+print("After swapping")
+print("A= " ,A," B=",B)
