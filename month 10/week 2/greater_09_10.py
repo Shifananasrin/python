111a3bec279aa267d865e8e6559af5a837512a72
Shifananasrin <shifananasrin941@gmail.com>
2024-10-09 11:21:10 +0530

    Update greater_09_10.py

--- month 10/week 2/greater_09_10.py
+++ month 10/week 2/greater_09_10.py
@@ -1,9 +1,9 @@
 A=int(input("enter the first number:"))
 B=int(input("enter the second number:"))
 C=int(input("enter the third  number:"))
-if(A>B):
+if(A>B  and A>C):
     print(A ,"is greater")
-elif(B>C):
-    print(B, "is greater")
+elif(B>C and B>C):
+        print(B, "is greater")
 else:
     print(C," is greater")
