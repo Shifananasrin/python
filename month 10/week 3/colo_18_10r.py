d6b1fdda51a615b6b0ff0c2634d315ecef58d222
Shifananasrin <shifananasrin941@gmail.com>
2024-10-23 11:54:41 +0530

    Update colo_18_10r.py

--- month 10/week 3/colo_18_10r.py
+++ month 10/week 3/colo_18_10r.py
@@ -1,6 +1,10 @@
 count = int(input("How much colors :"))
 colors=[]
+
 for i in range(count):
+    
     colors.append(input("Color : "))
-print("first color in list ",colors[0]," and  second color in list",colors[-1])
+print("list of color is",colors)
+print("first color in list  and  second color in list",[colors[0],colors[-1]])
+
 
