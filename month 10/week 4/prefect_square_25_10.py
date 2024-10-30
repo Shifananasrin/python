2548e456d6f93e94a0fa8113821f9b840fdb9b86
Shifananasrin <shifananasrin941@gmail.com>
2024-10-30 10:51:47 +0530

    Add files via upload

---
+++ month 10/week 4/prefect_square_25_10.py
@@ -0,0 +1,17 @@
+lower = int(input("Enter lower limit: "))
+upper = int(input("Enter upper limit: "))
+print("Perfect squares:")
+a=[]
+for i in range(lower, upper + 1):
+    j = int(i ** 0.5)
+    if j * j == i:
+     a.append(i)
+print(a)
+
+print("Even perfect squares:")
+c=[]
+for i in range(lower, upper + 1):
+    j = int(i ** 0.5)
+    if j * j == i and i % 2 == 0:
+        c.append(i)
+        print(c)
