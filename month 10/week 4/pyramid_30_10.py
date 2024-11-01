cbbd743c8f8a2305d775d98b301dd6c017d7ce0f
Shifananasrin <shifananasrin941@gmail.com>
2024-11-01 15:46:08 +0530

    Update pyramid_30_10.py

--- month 10/week 4/pyramid_30_10.py
+++ month 10/week 4/pyramid_30_10.py
@@ -1,5 +1,10 @@
 num=5
 for i in range(1,num+1):
-    print("* " * i)
+    for j in range(i):
+        print("* ",end=" ")
+    print()
+        
 for i in range(num -1,0,-1):
-    print("* " * i)
+    for i in range(i):
+       print("* ",end=" ")
+    print()
