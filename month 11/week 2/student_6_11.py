32289e4996ba0c2c763f2b6b78c2eb1d73c2dddc
Shifananasrin <shifananasrin941@gmail.com>
2024-11-06 15:22:48 +0530

    Update student_6_11.py

--- month 11/week 2/student_6_11.py
+++ month 11/week 2/student_6_11.py
@@ -6,9 +6,13 @@ student = {
     "department": "Computer Science",
     "semester": "S2"
 }
-print("student details",student)
+print("student details\n",student)
+
 print()
-student["total_mark"] = 82 
+total_marks=int(input("enter the mark:"))
+student["total_mark"]=total_marks
+print("student details with total mark\n",student)
+
 print()
 if student["total_mark"] >= 90:
     student["grade"] = "A"
@@ -22,8 +26,9 @@ elif student["total_mark"] >= 50:
     student["grade"] = "P"
 else:
     student["grade"] = "F"
-print(student)
+print("student details with grade\n",student)
 print()
 del student["roll_number"]
+print("student details  by deleting roll no\n",student)
 
-print(student)
+print()
